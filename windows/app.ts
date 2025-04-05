ff82750fc8f09f6594ee006fa62c5980e66b8e7f
RoMan-robots <romkozhan@gmail.com>
2025-04-06 00:07:08 +0300

    config

--- windows/app.ts
+++ windows/app.ts
@@ -1,10 +1,36 @@
+
 // @ts-ignore
-import screenCapture from './addons/screen_capture.node';
+import * as fs from 'fs';
+// @ts-ignore
+import * as napi from './screen_capture.node'; 
 
-(screenCapture as any).startStream((frame: any) => {
-    console.log('Frame:', frame);
-});
+let outputStream: fs.WriteStream | null = null;
 
+function startStreaming() {
+    console.log('Починаємо трансляцію відео...');
+    napi.start((frame: any) => {
+        const buffer = frame.data as Buffer;
+
+        if (outputStream) {
+            outputStream.write(buffer);
+        }
+    });
+}
+
+// Записуємо відео в файл
+function startRecording() {
+    outputStream = fs.createWriteStream('output_video.raw'); // Відео буде збережено в raw форматі
+    console.log('Запис почався...');
+}
+
+// Зупиняємо запис через 5 секунд
 setTimeout(() => {
-    (screenCapture as any).stopStream();
+    console.log('Запис завершено');
+    if (outputStream) {
+        outputStream.end();
+    }
 }, 5000);
+
+// Починаємо захоплення та запис
+startRecording();
+startStreaming();
